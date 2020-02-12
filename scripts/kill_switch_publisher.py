12ab06879be27c79dfcc29c57aaf8f612af7c45d
OSU UWRT <osu.uwrt@gmail.com>
2020-02-12 10:31:38 -0500

    Added kill switch

---
+++ scripts/kill_switch_publisher.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import rospy
+from riptide_msgs.msg import SwitchState
+from std_msgs.msg import Header
+class killSwitchPublisher():
+    def __init__(self):
+        self.pub = rospy.Publisher("state/switches", SwitchState, queue_size=10)
+        self.timer = rospy.timer.Timer(rospy.Duration(0.2), self.tick)
+
+    def tick(self, event):
+        self.pub.publish(SwitchState(Header(), True, False, False, False, False, False))
+
+if __name__ == '__main__':
+    rospy.init_node('kill_switch_publisher')
+    killSwitchPublisher()
+    rospy.spin()
