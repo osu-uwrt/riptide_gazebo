e954f47e63ff0e3a55048ea6d4efcee593ba7e76
OSU UWRT <osu.uwrt@gmail.com>
2020-07-12 23:24:17 -0400

    Fixing config read

--- scripts/thrust_remap.py
+++ scripts/thrust_remap.py
@@ -4,19 +4,21 @@ import rospy
 import yaml
 import math
 import numpy
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float32MultiArray, Header
 from uuv_gazebo_ros_plugins_msgs.msg import FloatStamped
 
 class ThrustConverter(object):
 
     def __init__(self):
-        self._vehicle_config_file = rospy.get_param("~vehicle_config")
+        self._vehicle_config_path = rospy.get_param("~vehicle_config")
         self._pubs = {}
         
-        with open(self._vehicle_config_file, 'r') as stream:
-            self._coeff = yaml.safe_load(stream)['thruster']['rotor_constant']
+        with open(self._vehicle_config_path, 'r') as stream:
+            config = yaml.safe_load(stream)
+            self.num_of_thrusters = len(config['thrusters'])
+            self._coeff = config['thruster']['rotor_constant']
 
-        for i in range(len(self._vehicle_config_file["thrusters"])):
+        for i in range(self.num_of_thrusters):
             input_topic = "thrusters/%d/input" % i
             pub = rospy.Publisher(input_topic, FloatStamped, queue_size=1)
             self._pubs[i] = pub
@@ -25,14 +27,14 @@ class ThrustConverter(object):
 
     def command_cb(self, msg):
         """Convert thrust to angular velocity commands for uuv_thruster_ros_plugin"""
-        for i in range(len(self._vehicle_config_file["thrusters"])):
+        for i in range(self.num_of_thrusters):
             cmd = FloatStamped()
             force = msg.data[i]
 
             # Compute angular velocity from force
             sign = 1 if force >= 0 else -1
             ang_vel = sign * math.sqrt(abs(force / self._coeff))
-            cmd.header = msg.header
+            cmd.header = Header()
             cmd.data = ang_vel
             self._pubs[i].publish(cmd)
 
