942417635cb86dd21bd01bc6beb4a8d4c1ffc44a
noahl1 <noahjlimes1@gmail.com>
2020-07-14 23:36:27 -0400

    automation_handler works with bad practice transdec world updated again

--- scripts/automation_handler.py
+++ scripts/automation_handler.py
@@ -2,24 +2,22 @@
 import roslaunch
 import rospy
 
+num_runs = 3
 package = 'riptide_gazebo'
 launch_file = 'world.launch'
 
 uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
 roslaunch.configure_logging(uuid)
 
-num_runs = 3
-
-
 for i in range(num_runs):
-    cli_args = [package, launch_file, 'world:=run' + str(i)]
+    arg = 'world:=run' + str(i)
+    cli_args = [package, launch_file, arg]
     roslaunch_args = cli_args[2:]
     roslaunch_file = roslaunch.rlutil.resolve_launch_arguments(cli_args)[0]
-    print roslaunch_file
-    launch_file = [(roslaunch_file, roslaunch_args)] 
-    launch = roslaunch.parent.ROSLaunchParent(uuid, launch_file)
-
+    exec('launch_file' + str(i) + ' = [(roslaunch_file, roslaunch_args)]')
+    exec('launch' + str(i) + '= roslaunch.parent.ROSLaunchParent(uuid, launch_file' + str(i) + ')')
     rospy.loginfo("started")
-    launch.start()
+    exec('launch' + str(i) + '.start()')
     rospy.sleep(60)
-    launch.shutdown()
+    exec('launch' + str(i) + '.shutdown()')
+    rospy.sleep(10)
