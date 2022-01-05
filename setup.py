5d0b32815c8d1ba186cdd4b9e734f95cca27b9c6
Cole Tucker <cjtucker321@gmail.com>
2022-01-04 19:50:27 -0500

    fix worlds and models for launching

--- setup.py
+++ setup.py
@@ -4,18 +4,33 @@ from glob import glob
 
 package_name = 'riptide_gazebo2'
 
-setup(
-    name=package_name,
-    version='0.0.1',
-    packages=[package_name],
-    data_files=[
+# bit of a hack to grab all of the model file dirs correctly
+def glob_model_paths():
+    result = {}
+    for root, dirs, files in os.walk("models", topdown=True):
+        for name in files:
+            shareDir = os.path.join('share', package_name, root)
+            if(not shareDir in result):
+                result[shareDir] = []
+            result[shareDir].append(os.path.join(root, name))
+
+    return result.items()
+
+data_files = []
+data_files.extend(glob_model_paths())
+data_files.extend([
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch*')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
-        (os.path.join('share', package_name, 'models'), glob('models/**/.*', recursive=True)),
-    ],
+    ])
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='OSU UWRT',
