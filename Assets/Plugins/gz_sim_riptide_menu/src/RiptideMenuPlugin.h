4560c552a5b59056fbaf7feefbd2effd2bcbca48
spyalexs <spyalexs@gmail.com>
2023-02-16 19:58:59 -0500

    Fixed Gazebo

---
+++ Assets/Plugins/gz_sim_riptide_menu/src/RiptideMenuPlugin.h
@@ -0,0 +1,18 @@
+#pragma once
+
+//Reference::https://github.com/gazebosim/gz-gui/tree/gz-gui7/examples/plugin/gz_components
+
+#include <gz/gui/Plugin.hh>
+
+
+namespace gz::sim{
+
+    class RiptideMenuPlugin : public gz::gui::Plugin{
+        Q_OBJECT;
+        public:
+
+        RiptideMenuPlugin() = default;
+
+
+    }
+}
